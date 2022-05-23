d36fc705cc409445f5179ed022acc92673a3fb32
kuty007 <asaf701@gmail.com>
2022-05-23 10:36:36 +0300

    first commit

---
+++ Analyst.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class analyst1
+{
+	public string Name
+	prstring passwored 
+	public analyst1()
+
+
+	{
+
+
+	}
+}
---
+++ User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+abstract class user1
+{
+	public abstract string Name { get; }
+	public abstract string Password { get; }
+	public user1()
+	{
+		Name = Console.readline
+	}
+}
